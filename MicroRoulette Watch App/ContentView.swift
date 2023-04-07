df502fd71990d644bf89c2d2d3a9ea12815c0ced
Daniil Popov <lalabuy9948@gmail.com>
2023-04-07 16:11:30 +0200

    Add even/odd and slot picker

--- MicroRoulette Watch App/ContentView.swift
+++ MicroRoulette Watch App/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    private var numbers = 0...13
+
+    @State private var oddEven = ["odd", "even"]
+    @State private var number: Int = 0
+    
+    @State private var scrollAmount = 0.0
+    @State private var progress = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            HStack {
+
+                Picker("", selection: $oddEven) {
+                    ForEach(oddEven, id: \.self) { item in Text(item) }
+                }
+                .padding()
+                .focusable(true)
+    
+                Picker("", selection: $number) {
+                    ForEach(numbers, id: \.self) { _ in Text("\(number)") }
+                }
+                .padding()
+                .focusable(true)
+                
+                
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 65)
+            .padding(.top, -30)
+            
+//            ZStack
+//            {
+//                Circle ()
+//                    .stroke (lineWidth: 10)
+//                    .opacity (0.2)
+//                    .foregroundColor (.white)
+//                    .frame (width: 120)
+//
+//                Circle()
+//                    .offset (y: -60)
+//                    .foregroundColor(.white)
+//                    .frame (width: 10)
+//                    .rotationEffect(Angle (degrees: progress))
+//                    .focusable (true)
+//                    .digitalCrownRotation($scrollAmount, from: 0, through: 360, by: 1, isContinuous: false)
+//                    .onChange(of: scrollAmount) { value in
+//                        self.progress = value
+//                    }
+//
+//                Circle()
+//                    .offset (y: -60)
+//                    .foregroundColor(.white)
+//                    .frame (width: 10)
+//                    .rotationEffect(Angle (degrees: -progress))
+//                    .focusable (true)
+//                    .digitalCrownRotation($scrollAmount, from: 0, through: 360, by: 1, isContinuous: false)
+//                    .onChange(of: scrollAmount) { value in
+//                        self.progress = value
+//                    }
+//            }
+
         }
-        .padding()
     }
 }
 
