be87c1437b00d2e15e434428efa121f09a0ce054
Daniil Popov <lalabuy9948@gmail.com>
2023-04-07 10:32:57 +0200

    first commit

---
+++ MicroRoulette Watch App/MicroRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroRouletteApp.swift
+//  MicroRoulette Watch App
+//
+//  Created by Daniil Popov on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MicroRoulette_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
