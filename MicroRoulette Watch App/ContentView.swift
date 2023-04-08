b237720fe805494b6b431e02e73c56147c78effd
Daniil Popov <lalabuy9948@gmail.com>
2023-04-08 16:30:49 +0200

    Add labels

--- MicroRoulette Watch App/ContentView.swift
+++ MicroRoulette Watch App/ContentView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
-
+import WatchShaker
 
 struct ContentView: View {
     private var numbers = 0...14
+    private var data: [Double] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] // 13
+    private var slots: [Int] =   [3, 8, 0, 5, 12,1, 4, 9, 6,11, 2, 7,10]
+    private var segments = 13
+    
+    private var weakSpin = 360 * 3
+    private var midSpin = 360 * 5
+    private var strongSpin = 360 * 7
+    private var randDegree = Int.random(in: 1...360)
 
     @State private var oddEven = ["", "odd", "even"]
     @State private var number: Int = 0
@@ -17,11 +25,14 @@ struct ContentView: View {
     @State private var scrollAmount = 0.0
     @State private var progress = 0.0
     
-    let data: [Double] = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-//    let labels = []
+    @State private var spin = false
+    
+    @State private var shaker:WatchShaker = WatchShaker(shakeSensibility: .shakeSensibilityNormal, delay: 0.2)
 
     var body: some View {
+        
         VStack(spacing: 0) {
+            
             HStack {
                 Picker("", selection: $oddEven) {
                     ForEach(oddEven, id: \.self) { item in Text(item) }
@@ -37,34 +48,63 @@ struct ContentView: View {
 
             }
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 65)
-            .padding(.top, -20)
+            .padding(.top, -10)
+            
             
             ZStack {
                 Circle()
                     .stroke(lineWidth: 10)
                     .opacity(0.2)
                     .foregroundColor(.white)
-                    .frame(width: 120)
+                    .frame(width: 150, height: 150)
                 
-                PieChart(data: data)
-                    .frame(width: 110)
+                PieChart(data: data, slots: slots)
+                    .frame(width: 140)
                     .rotationEffect(Angle (degrees: -progress))
                     .focusable(true)
                     .digitalCrownRotation($scrollAmount, from: 0, through: 360, by: 1, sensitivity: .high, isContinuous: true)
                     .onChange(of: scrollAmount) { value in
                         self.progress = value
                     }
+                    // spin starts with tap on roulette
+//                    .onTapGesture {
+//                        self.spin.toggle()
+//                    }
 
                 Circle()
-                    .offset(y: -60)
+                    .offset(y: -75)
                     .foregroundColor(.white)
                     .frame(width: 10)
+                    .rotationEffect(.degrees(spin ? Double((midSpin + Int.random(in: 1...360))) : 0))
+                    .animation(
+                        .spring(response: 2, dampingFraction: 0.9, blendDuration: 0)
+                        .repeatCount(1, autoreverses: false)
+                        , value: self.spin
+                    )
+                    .animation(
+                        .linear(duration: 3)
+                        .delay(Double.random(in: 0...3))
+                        .repeatCount(1, autoreverses: false)
+                        , value: self.spin
+                    )
+                    .animation(
+                        .easeOut(duration: 0.5)
+                        .delay(5.0)
+                        .repeatCount(1, autoreverses: false)
+                        , value: self.spin
+                    )
+                    // start spin with Digital Crown
                     .rotationEffect(Angle (degrees: progress))
                     .focusable (true)
                     .digitalCrownRotation($scrollAmount, from: 0, through: 360, by: 1, sensitivity: .high, isContinuous: true)
                     .onChange(of: scrollAmount) { value in
                         self.progress = value
+                        
+                        if value > 180 {
+                            self.spin.toggle()
+                        }
                     }
+                
             }
             .padding(.top, 10)
         }
@@ -79,7 +119,8 @@ struct ContentView_Previews: PreviewProvider {
 
 struct PieChart: View {
     let data: [Double]
-    let lineWidth: CGFloat = 1
+    let lineWidth: CGFloat = 0
+    let slots: [Int]
     
     private var total: Double {
         data.reduce(0, +)
@@ -94,12 +135,28 @@ struct PieChart: View {
         ZStack {
             ForEach(0..<data.count, id: \.self) { index in
                 let startAngle = index == 0 ? .zero : angle(for: data[0..<index].reduce(0, +))
-                let endAngle = angle(for: data[0...index].reduce(0, +))
-                
-                Sector(startAngle: startAngle, endAngle: endAngle)
-                    .fill(Color(hue: Double(index) / Double(data.count), saturation: 1, brightness: 0.3))
-                    .overlay(Sector(startAngle: startAngle, endAngle: endAngle)
-                                .stroke(Color.black, lineWidth: lineWidth))
+                let endAngle = angle(for: data[0..<index].reduce(0, +))
+                let midAngle = startAngle + (endAngle - startAngle) / 1.3 // midpoint of the sector
+                let labelRadius = 120 / 2.5 // radius for the label to be placed
+
+                ZStack {
+//                    @todo: Add proper sectors
+//                    Sector(startAngle: startAngle, endAngle: endAngle)
+//                        .fill(Color(hue: Double(index) / Double(data.count), saturation: 1, brightness: 0.3))
+//                        .overlay(
+//                            Sector(startAngle: startAngle, endAngle: endAngle)
+//                                .stroke(Color.white, lineWidth: lineWidth)
+//                        )
+                    
+                    // Circular text label
+                    Text("\(slots[index])")
+                        .font(.system(size: 10))
+                        .fontWeight(Font.Weight.heavy)
+                        .foregroundColor(.white)
+                        .rotationEffect(midAngle)
+                        .offset(x: labelRadius * sin(midAngle.radians),
+                                y: -labelRadius * cos(midAngle.radians))
+                }
             }
         }
     }
